c4fba03fccbfe92b4aa694d07f08d247379d8dc0
Brian Harder <briankharder@gmail.com>
2024-07-23 22:07:57 -0700

    meta tags

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Nav = () => {
     return (
-        <h1>Nav</h1>
+        <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/menu">Menu</Link></li>
+            <li><Link to="/reservations">Reservations</Link></li>
+            <li><Link to="/order-online">Order Online</Link></li>
+        </ul>
     );
 };
 
